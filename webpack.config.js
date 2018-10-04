70b297656109446afdccba1880af78c89202c94e
danncortes <danncortes@gmail.com>
2018-10-04 16:39:18 -0500

    Improving Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+function plugins(argv){
+  const plugins = [
+    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ];
+
+  if(argv.mode === 'development'){
+    plugins.push(new CleanWebpackPlugin(['dist']))
+  }
+  return plugins;
+}
+
+
+module.exports = (env, argv) => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    plugins: [
+      ...plugins(argv)
+    ]
   }
 };
