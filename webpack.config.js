abdc8e212d13b8a014117726cf2b15d01a8c76f2
Daniel Cortes <danncortes@gmail.com>
2018-10-05 00:31:22 -0500

    Fixing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,59 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-function plugins(argv){
-  const plugins = [
+function plugins(argv) {
+  const plugs = [
     new HtmlWebpackPlugin(),
-    new HtmlWebpackPlugin({  // Also generate a test.html
+    new HtmlWebpackPlugin({ // Also generate a test.html
       filename: 'index.html',
-      template: 'src/index.html'
-    })
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ];
 
-  if(argv.mode === 'development'){
-    plugins.push(new CleanWebpackPlugin(['dist']))
+  if (argv.mode === 'development') {
+    plugs.push(
+      new CleanWebpackPlugin(['dist']),
+    );
   }
-  return plugins;
+  return plugs;
 }
 
-
-module.exports = (env, argv) => {
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
-    },
-    plugins: [
-      ...plugins(argv)
-    ]
-  }
-};
+module.exports = (env, argv) => ({
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+            },
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    ...plugins(argv),
+  ],
+});
