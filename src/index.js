b3672e71e8ace737f8ee09f423821fccbe057fea
Daniel Cortes <danncortes@gmail.com>
2019-01-05 17:52:31 -0500

    Configuring Linter and Prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "./style.scss";
-import myModule from "./myModule";
+import './style.scss';
+import myModule from './myModule';
 
 export default function firstFunction(         ) {
-  return "Hello World";
+  return 'Hello World';
 }
 
 myModule();
